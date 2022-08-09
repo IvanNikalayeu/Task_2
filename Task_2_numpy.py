1b0c845369964289ff931e5b6f0e811e3b363755
IvanNikalayeu <ivan.nikalayeu@innowise-group.com>
2022-08-09 16:02:39 +0300

    Update Task_2_numpy.py

--- Task_2_numpy.py
+++ Task_2_numpy.py
@@ -1,6 +1,21 @@
 import numpy as np
 
-arr_random = np.arange(1,16)
-print(arr_random)
+
 
 # Дан случайный массив, поменять знак у элементов, значения которых между 3 и 8
+# arr_random = np.arange(1, 16)
+# print(arr_random)
+# arr_random[(3 < arr_random) & (arr_random < 8)] *= -1
+# print(arr_random)
+
+# Заменить максимальный элемент случайного массива на 0
+# arr_random = np.arange(30)
+# print(arr_random)
+# arr_random[arr_random.argmax()] = 0
+# print(arr_random)
+
+# Построить прямое произведение массивов (все комбинации с каждым элементом). На вход подается двумерный массив
+arr = np.empty((4, 6, 3), dtype='int32')
+print(arr)
+
+
